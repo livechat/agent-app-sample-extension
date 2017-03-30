7efc855e106dd51a25691af44b6fb1ef83d68278
Filip Jaskolski <f.jaskolski@livechatinc.com>
2017-03-30 12:04:47 +0200

    updated scripts, printing customer_profile event source

--- web/js/script.js
+++ web/js/script.js
@@ -25,6 +25,7 @@
             $('#visitorEmail').html(data.email);
             $('#chatID').html(data.chat.id);
             $('#groupID').html(data.chat.groupID);
+            $('#source').html(data.source);
             console.log(data.chat);
         }
     }
